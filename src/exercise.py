70de6e8826685f72ba206a8edd60be67c74752b5
MrSullivan <rsullivan@stcadocs.com>
2021-02-24 22:51:17 +0000

    added two integar variables and print command pulling back to string.

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,8 @@
 def main():
     #write your code below this line
+    first_number = int(input("Give the first number:"))
+    second_number = int(input("Give the second number:"))
+    print(str(first_number) + "*" + str(second_number) + "=" + str(first_number*second_number))
 
 if __name__ == '__main__':
     main()
