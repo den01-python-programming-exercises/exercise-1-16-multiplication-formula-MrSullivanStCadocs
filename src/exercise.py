39e8ac940e2b643bc28b047d5b750ff4401eb69b
MrSullivan <rsullivan@stcadocs.com>
2021-02-24 22:53:01 +0000

    added spaces.

--- src/exercise.py
+++ src/exercise.py
@@ -2,7 +2,7 @@ def main():
     #write your code below this line
     first_number = int(input("Give the first number:"))
     second_number = int(input("Give the second number:"))
-    print(str(first_number) + "*" + str(second_number) + "=" + str(first_number*second_number))
+    print(str(first_number) + " * " + str(second_number) + " = " + str(first_number*second_number))
 
 if __name__ == '__main__':
     main()
